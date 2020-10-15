703b1b5e8952676560370c80c8801c2bf63d9314
henipatel8 <43182867+henipatel8@users.noreply.github.com>
2020-10-15 15:27:46 -0400

    london transit updated

--- kepler/london_transit/clean_bus.py
+++ kepler/london_transit/clean_bus.py
@@ -9,6 +9,7 @@ except:
 # %% Import all packages
 import pandas as pd
 
+
 # %% Import Data
 stops = pd.read_csv('./data/stops.csv')
 trips = pd.read_csv('./data/trips.csv')
@@ -17,15 +18,43 @@ stop_times = pd.read_csv('./data/stop_times.csv')
 print(stops, trips, stop_times)
 
 # %% Merge stops into stop_times by stop_id
-
 stops_merge = stop_times.merge(
-    stops[[ 'stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'stop_code']], on='stop_id')
+    stops[['stop_id', 'stop_name', 'stop_lat', 'stop_lon', 'stop_code']], on='stop_id')
 stops_merge.head()
-# stop_merge.shape
+
 
 # %% Merge trips into stops_merge by trip_id
 trips_merge = stops_merge.merge(
     trips[['trip_id', 'route_id']], on='trip_id')
 trips_merge.head()
+
+# %%Customize function
+def validate_time(date_str):
+    x = int(date_str, split(':', 1)[0])
+    if x >= 24:
+        return str(x%24) + date_str[2:]
+    else:
+        return date_str
+
+# %% Clean the time format
+trips_merge['arrival_time'] = trips_merge['arrival_time'].apply(
+    lambda x: x.replace(' ', '0')
+)
+
+trips_merge['departure_time'] = trips_merge['departure_time'].apply(
+    lambda x: x.replace(' ', '0')
+)
+
+
+
+# %% Convert arrival_time and departure_time to be time
+trips_merge['arrival_time'] = pd.to_datetime(
+    trips_merge['arrival_time'], format='%H:%M:%S').dt.time
+trips_merge['departure_time'] = pd.to_datetime(
+    trips_merge['departure_time'], format='%H:%M:%S').dt.time
+
+
+# %% Export to new csv
+trips_merge.to_csv('./data/full_trips.csv')
 
 # %%
